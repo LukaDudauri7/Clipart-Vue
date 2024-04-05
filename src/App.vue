4073c64702e8c41f61ccc3d5d063c4d2b13b9553
LukaDudauri7 <lukadudauri01@gmail.com>
2024-04-05 17:19:33 +0400

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,42 @@
 <template>
-<div>
   <HeaderComponent />
-</div>
+  <MainComponent />
 </template>
 
 <script>
 import HeaderComponent from './components/header.vue'
+import MainComponent from './components/main.vue'
 export default {
   name: 'App',
   components: {
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   }
 }
 </script>
 
 <style>
+@font-face {
+  font-family: 'NotoSans';
+  src: url('./fonts/NotoSansGeorgian.ttf') format('woff2')
+}
+html{
+  height: 100vh;
+  width: 100vw; 
+  background-color: #F0F4F5;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px; 
   background-color: #f0f0f0;
+  text-align: center;
+  width: 100%; */
+}
+body{
+  margin: 0;
+  padding: 0;
+  /* min-height: 100vh; */
 }
 </style>
+
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,15 +1,79 @@
 <template>
-  <div>
-    <h1>This is Header</h1>
-  </div>
+    <div class="container">
+      <div class="center_container"> 
+        <div class="navbar-menu">
+          <img src="../assets/Logo.png" class="navbar-logo">
+          <div class="navbar-end">
+            <a class="navbar-item" href="#">პოლიტიკა</a>
+            <a class="navbar-item" href="#">საზოგადოება</a>
+            <a class="navbar-item" href="#">სამართალი</a>
+            <a class="navbar-item" href="#">ბიზნესი & ეკონომიკა</a>
+          </div>
+          <img src="../assets/instagram.png" class="navbar-logo">
+          <img src="../assets/facebook.png" class="navbar-logo">
+          <input placeholder="ძიება"> 
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HeaderComponent'
+  name: 'HeaderComponent',
+   data() {
+    return {
+      // isMenuOpen: false
+    };
+  },
+  // methods: {
+  //   toggleMenu() {
+  //     this.isMenuOpen = !this.isMenuOpen;
+  //   }
+  // }
 }
 </script>
 
 <style scoped>
-
+.container {
+  background-color: white;
+  width: 100%;
+  height: 112px;
+  display: flex;
+  justify-content: center;
+}
+.center_container{
+  max-width: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.navbar-logo {
+  max-height: 2rem;
+}
+.navbar-menu {
+  display: flex;
+}
+a {
+  text-decoration: none;
+  color: #657178;
+  line-height: 24px;
+  font-size: 16px;
+  font-weight: 500;
+  font-family: 'NotoSans';
+}
+input{
+  font-family: 'NotoSans';
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+}
+.navbar-end{
+  width: 85%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+img{
+  cursor: pointer;
+}
 </style>
