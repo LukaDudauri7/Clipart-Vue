5401109c21059cbcf5aa82056b274ee6e0d86267
LukaDudauri7 <lukadudauri01@gmail.com>
2024-04-12 18:32:11 +0400

    .

--- src/components/news.vue
+++ src/components/news.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="news-container">
-    <div class="img-container"></div>
+    <h1>ყველა სიახლე</h1>
+    <div class="paragraphs">
+      <div v-for="index in 30" :key="index">
+        <ParagraphComponent />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ParagraphComponent from "./paragraph.vue";
 export default {
   name: "NewsComponent",
+  components: {
+    ParagraphComponent,
+  },
 };
 </script>
 
@@ -14,8 +23,28 @@ export default {
 .news-container {
   margin-top: 40px;
   width: 304px;
-  height: 618px;
+  height: 606px;
   background-color: white;
   border-radius: 20px;
+  overflow: hidden;
+}
+.paragraphs {
+  overflow: auto;
+  height: calc(100% - 60px); /* Adjusted height to accommodate h1 */
+}
+h1 {
+  font-size: 20px;
+  line-height: 28px;
+  font-family: "NotoSans";
+  color: #1d2d35;
+  margin-left: 20px;
+  margin-top: 20px;
+}
+::-webkit-scrollbar {
+  width: 4px;
+}
+::-webkit-scrollbar-thumb {
+  background: #657178;
+  border-radius: 6px;
 }
 </style>
---
+++ src/components/paragraph.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="news">
+    <div class="news-header">
+      <img src="../assets/Vector.png" />
+      <div class="time">7:30</div>
+      <div class="rectangle"></div>
+      <div class="time">02.01.2023</div>
+    </div>
+    <p>დღეიდან ასაწონად განკუთვნილ პოლიეთილენის პარკები აიკრძალა</p>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ParagraphComponent",
+};
+</script>
+
+<style scoped>
+.news{
+  margin-top: 25px;
+}
+.news-header {
+  display: flex;
+  width: 272px;
+  margin-left: 20px;
+  gap: 5px;
+  align-items: center
+}
+p {
+  font-weight: 400;
+  color: #1d2d35;
+  font-size: 14px;
+  font-family: "NotoSans";
+  line-height: 20px;
+  width: 272px;
+  text-align: left;
+  margin-left: 20px;
+  margin-top: 0px;
+}
+.time{
+  color: #585EE3;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 16px;
+  font-family: "NotoSans";
+}
+.rectangle{
+  background-color: #585EE3;
+  width: 4px;
+  height: 4px;
+  border-radius: 50%;
+}
+</style>
