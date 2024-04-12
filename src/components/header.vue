607250eb9c47c1792290a7e9dac68f71327835dd
LukaDudauri7 <lukadudauri01@gmail.com>
2024-04-12 17:06:19 +0400

    .

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,26 +1,31 @@
 <template>
-    <div class="container">
-      <div class="center_container"> 
-        <div class="navbar-menu">
-          <img src="../assets/Logo.png" class="navbar-logo">
+  <div class="container">
+    <div class="center_container">
+      <div class="navbar-menu">
+        <div class="left-side">
+          <img src="../assets/Logo.png" />
           <div class="navbar-end">
             <a class="navbar-item" href="#">პოლიტიკა</a>
             <a class="navbar-item" href="#">საზოგადოება</a>
             <a class="navbar-item" href="#">სამართალი</a>
             <a class="navbar-item" href="#">ბიზნესი & ეკონომიკა</a>
+            <a class="navbar-item" href="#">სხვა</a>
           </div>
-          <img src="../assets/instagram.png" class="navbar-logo">
-          <img src="../assets/facebook.png" class="navbar-logo">
-          <input placeholder="ძიება"> 
+        </div>
+        <div class="right-side">
+          <img src="../assets/instagram.png" class="navbar-logo" />
+          <img src="../assets/facebook.png" class="navbar-logo" />
+          <input placeholder="ძიება" />
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HeaderComponent',
-   data() {
+  name: "HeaderComponent",
+  data() {
     return {
       // isMenuOpen: false
     };
@@ -30,7 +35,7 @@ export default {
   //     this.isMenuOpen = !this.isMenuOpen;
   //   }
   // }
-}
+};
 </script>
 
 <style scoped>
@@ -41,7 +46,7 @@ export default {
   display: flex;
   justify-content: center;
 }
-.center_container{
+.center_container {
   max-width: 90%;
   display: flex;
   justify-content: center;
@@ -52,6 +57,7 @@ export default {
 }
 .navbar-menu {
   display: flex;
+  gap: 60px;
 }
 a {
   text-decoration: none;
@@ -59,21 +65,31 @@ a {
   line-height: 24px;
   font-size: 16px;
   font-weight: 500;
-  font-family: 'NotoSans';
+  font-family: "NotoSans";
 }
-input{
-  font-family: 'NotoSans';
+input {
+  font-family: "NotoSans";
   font-size: 14px;
   font-weight: 400;
   line-height: 20px;
 }
-.navbar-end{
+.navbar-end {
   width: 85%;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  gap: 20px;
 }
-img{
+img {
   cursor: pointer;
+}
+.left-side{
+  display: flex;
+  gap: 18px;
+}
+.right-side{
+  display: flex;
+  gap: 12px;
+  align-items: center;
 }
 </style>
