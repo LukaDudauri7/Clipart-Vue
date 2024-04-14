6218511b6650524b67a140c60989c38d4c0c13c7
LukaDudauri7 <lukadudauri01@gmail.com>
2024-04-14 16:04:38 +0400

    add images

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,28 +1,39 @@
 <template>
   <div class="center-container">
     <div class="img-container">
-    <div class="swiper">
-      <div class="swiper-wrapper">
-      <img src="../assets/region.jpg" class="img-class swiper-slide" />
-        <!-- <div class="swiper-slide">Slide 1</div> -->
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
+      <div class="swiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <img src="../assets/downtown.jpg" class="img-class" />
+          </div>
+            <div class="swiper-slide">
+          <img src="../assets/region.jpg" class="img-class" />
+            </div>
+            <div class="swiper-slide">
+          <img src="../assets/buildings.jpg" class="img-class" />
+            </div>
+            <div class="swiper-slide">
+          <img src="../assets/chicago.jpg" class="img-class" />
+            </div>
+        </div>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-scrollbar"></div>
       </div>
-      <!-- If we need pagination -->
-      <div class="swiper-pagination"></div>
-      <!-- If we need navigation buttons -->
-      <div class="swiper-button-prev"></div>
-      <div class="swiper-button-next"></div>
-      <!-- If we need scrollbar -->
-      <div class="swiper-scrollbar"></div>
-    </div>
     </div>
     <div class="timeandfield">
       <div class="field">12.08.2023</div>
       <div class="field">პოლიტიკა</div>
     </div>
-    <div class="main-text">ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების ინფრასტრუქტურული პროექტები დაიწყება</div>
-    <div class="second-text">განთავსების (ლისტინგის) ორგანიზატორი სომხეთის ბანკი სს “არდშიბანკია”. ობლიგაციის ნომინალი - 1000 USD</div>
+    <div class="main-text">
+      ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების ინფრასტრუქტურული
+      პროექტები დაიწყება
+    </div>
+    <div class="second-text">
+      განთავსების (ლისტინგის) ორგანიზატორი სომხეთის ბანკი სს “არდშიბანკია”.
+      ობლიგაციის ნომინალი - 1000 USD
+    </div>
   </div>
 </template>
 
@@ -38,18 +49,13 @@ export default {
   mounted() {
     new Swiper(".swiper", {
       modules: [Navigation, Pagination],
-      // direction: "vertical",
-      // loop: true,
-      // If we need pagination
       pagination: {
         el: ".swiper-pagination",
       },
-      // Navigation arrows
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
-      // And if we need scrollbar
       scrollbar: {
         el: ".swiper-scrollbar",
       },
@@ -67,7 +73,7 @@ export default {
   border-radius: 20px;
 }
 .img-class {
-  width: 952px !important;
+  width: 952px;
   height: 406px;
   position: relative;
   left: calc((100% - 952px) / 2);
